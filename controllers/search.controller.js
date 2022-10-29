d4654cb601221fac47b4bc04301cf95ff70e3646
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-10-29 21:04:39 +0800

    feat: add search history model, routes and controller

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -59,6 +59,94 @@ class SearchController {
       process.exit(1)
     }
   }
+
+  static getSearchHistories = async (req, res, next) => {
+    if (req.session.user === undefined) {
+      res.status(401).json({
+        message: 'Unauthorized!'
+      })
+      return
+    }
+
+    try {
+      const searchHistories = await prisma.searchHistory.findMany({
+        where: {
+          userId: String(req.params.userId)
+        },
+        select: {
+          id: true,
+          type: true,
+          searchId: true,
+          slug: true,
+          image: true,
+          title: true,
+          description: true,
+          updatedAt: true,
+          userId: true
+        }
+      })
+      res.status(200).json(searchHistories)
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+  }
+
+  static storeSearchHistoryDishes = async (req, res, next) => {
+    if (req.session.user === undefined) {
+      res.status(401).json({
+        message: 'Unauthorized!'
+      })
+      return
+    }
+
+    try {
+      const store = await prisma.searchHistory.create({
+        data: {
+          type: 'DISHES',
+          searchId: req.body.searchId,
+          slug: req.body.slug,
+          image: req.body.image,
+          title: req.body.title,
+          description: req.body.description,
+          updatedAt: new Date(),
+          userId: req.body.userId
+        }
+      })
+      res.status(200).json(store)
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+  }
+
+  static storeSearchHistoryPeople = async (req, res, next) => {
+    if (req.session.user === undefined) {
+      res.status(401).json({
+        message: 'Unauthorized!'
+      })
+      return
+    }
+
+    try {
+      const store = await prisma.searchHistory.create({
+        data: {
+          type: 'PEOPLE',
+          searchId: req.body.searchId,
+          slug: req.body.slug,
+          image: req.body.image,
+          title: req.body.title,
+          description: req.body.description,
+          updatedAt: new Date(),
+          userId: req.body.userId
+        }
+      })
+      res.status(200).json(store)
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+  }
 }
 
 module.exports = SearchController
