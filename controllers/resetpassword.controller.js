51c18d0e5fdfcac98db9615e1bbb2723c4bb8f09
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-11-06 09:41:36 +0800

    feat: add forgot-password controller

---
+++ controllers/resetpassword.controller.js
@@ -0,0 +1,64 @@
+const createError = require("http-errors");
+require('express-async-errors');
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const jwt = require('jwt-simple')
+const emailJS = require('@emailjs/browser')
+
+class ResetPasswordController {
+  static forgotPassword = async (req, res, next) => {
+    try {
+      const { email } = req.body
+  
+      const foundUser = await prisma.user.findMany({
+        where: {
+          email: email
+        },
+        select: {
+          id: true,
+          email: true,
+          name: true
+        }
+      })
+  
+      if (!foundUser[0]) {
+        return res.status(400).json({
+          message: 'Email not found.'
+        })
+      }
+
+      const payload = { userId: foundUser[0].id }
+      const secret = process.env.JWT_SECRET
+      const token = jwt.encode(payload, secret)
+
+      const name = foundUser[0].name
+      const message = 'To reset your password in Rekados App paste this reset password code'
+      const reset_code = String(token)
+
+      const mail = await emailJS.send(
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
+        { name, email, message, reset_code },
+        process.env.EMAILJS_PUBLIC_KEY
+      )
+
+      if (!mail) {
+        return res.status(400).json({
+          message: 'Something went wrong, try again.'
+        })
+      }
+
+      res.status(200).json({
+        reset_code: String(reset_code),
+        message: 'Check your email to get the reset password code!'
+      })
+
+    } catch (e) {
+      next(createError(e.statusCode, e.message))
+      process.exit(1)
+    }
+  }
+}
+
+module.exports = ResetPasswordController
