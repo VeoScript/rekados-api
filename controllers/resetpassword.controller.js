cb592eb39da9bdc0b39522a271e96a3cdac94388
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-11-06 11:27:50 +0800

    refactor: forgot password controller

--- controllers/resetpassword.controller.js
+++ controllers/resetpassword.controller.js
@@ -4,7 +4,6 @@ require('express-async-errors');
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require('jwt-simple')
-const emailJS = require('@emailjs/browser')
 
 class ResetPasswordController {
   static forgotPassword = async (req, res, next) => {
@@ -34,24 +33,15 @@ class ResetPasswordController {
       const token = jwt.encode(payload, secret)
 
       const name = foundUser.name
-      const message = 'To reset your password in Rekados App paste this reset password code'
+      const mailMessage = 'To reset your password in Rekados App paste this reset password code'
       const reset_code = String(token)
 
-      await emailJS.send(
-        process.env.EMAILJS_SERVICE_ID,
-        process.env.EMAILJS_TEMPLATE_ID,
-        { name, email, message, reset_code },
-        process.env.EMAILJS_PUBLIC_KEY
-      ).then(() => {
-        res.status(200).json({
-          reset_code,
-          message: 'Check your email to get the reset password code!'
-        })
-      }).catch((error) => {
-        console.error(error)
-        return res.status(400).json({
-          message: String(error)
-        })
+      res.status(200).json({
+        email,
+        name,
+        mailMessage,
+        reset_code,
+        message: 'Check your email to get the reset password code!'
       })
 
     } catch (e) {
